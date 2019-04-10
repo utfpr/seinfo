589ae6b4a3884663b83f22fd006f4b46ffc54f41
henriko202 <henriko.foz@gmail.com>
2019-04-10 17:54:43 -0300

    adicionado UPDATE

--- backend-controle-seinfo/banco/dbconnection.js
+++ backend-controle-seinfo/banco/dbconnection.js
@@ -4,22 +4,10 @@ function execSQLQuery(sqlQry, res){
     const connection = mysql.createConnection({
       host     : 'localhost',
       port     : 3306,
-      user     : 'michel',
-      password : 'password',
+      user     : 'root',
+      password : 'root',
       database : 'seinfo'
     });
-<<<<<<< HEAD:backend-controle-seinfo/models/banco.js
-
-    connection.query(sqlQry, function(error, results, fields){
-        if(error)
-          res.json(error);
-        else
-          res.json(results);
-        connection.end();
-        console.log('executou!');
-    });
-  }
-=======
   
   connection.query(sqlQry, function(error, results, fields){
       if(error)
@@ -30,7 +18,6 @@ function execSQLQuery(sqlQry, res){
       console.log('executou!');
   });
 }
->>>>>>> f519244ab44efe59196dac9a0345d2000f142c05:backend-controle-seinfo/banco/dbconnection.js
 
 
 module.exports = {
--- backend-controle-seinfo/controllers/evento.js
+++ backend-controle-seinfo/controllers/evento.js
@@ -12,10 +12,11 @@ const executarDeletar = async (id,res) => {
     //banco.execSQLQuery('SELECT * FROM evento', res);
   };
 
-
-
-
-  //execSQLQuery(`INSERT INTO evento(nome, valor, descricao, data_horario_inicio, data_hora_fim ) VALUES('${nome}','${valor}','${descricao}','${dataFull_Ini}','${dataFull_fim}')`, res);
+const executarUpdate = async (nome, valor, descricao, data_horario_inicio, data_hora_fim, id, res) => {
+    console.log("Executar um UPDATE");
+    banco.execSQLQuery(`UPDATE evento set nome='${nome}',valor='${valor}',descricao='${descricao}',data_horario_inicio='${dataFull_Ini}',data_hora_fim='${dataFull_fim}' WHERE idEvento=` +id, res);
+    
+  };
 
 const executaPost = async(nome, valor, dataFull_Ini, dataFull_fim, descricao,res)=>{
     console.log("Executar POST");
@@ -26,4 +27,5 @@ module.exports = {
     executarSelection,
     executaPost,
     executarDeletar,
+    executarUpdate,
   };
