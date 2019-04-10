8855b333c7c25bb5406783268d936998c7e24c14
Michel Gomes <michels@alunos.utfpr.edu.br>
2019-04-10 17:43:06 -0300

    Mudando os nomes das Rotas do Front-End de Eventos para Evento

--- backend-controle-seinfo/banco/dbconnection.js
+++ backend-controle-seinfo/banco/dbconnection.js
@@ -8,7 +8,6 @@ function execSQLQuery(sqlQry, res){
       password : 'password',
       database : 'seinfo'
     });
-<<<<<<< HEAD:backend-controle-seinfo/models/banco.js
 
     connection.query(sqlQry, function(error, results, fields){
         if(error)
@@ -19,19 +18,6 @@ function execSQLQuery(sqlQry, res){
         console.log('executou!');
     });
   }
-=======
-  
-  connection.query(sqlQry, function(error, results, fields){
-      if(error)
-        res.json(error);
-      else
-        res.json(results);
-      connection.end();
-      console.log('executou!');
-  });
-}
->>>>>>> f519244ab44efe59196dac9a0345d2000f142c05:backend-controle-seinfo/banco/dbconnection.js
-
 
 module.exports = {
   execSQLQuery,
