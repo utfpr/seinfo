c67f3f07eb4852fdd784369ff5fbdfb7d9adcabf
Michel Gomes <michels@alunos.utfpr.edu.br>
2019-04-10 17:31:10 -0300

    Lendo lista de Eventos (Read), Inserindo Eventos(Create), Deletando Eventos(Delete)

--- backend-controle-seinfo/models/banco.js
+++ backend-controle-seinfo/models/banco.js
@@ -4,11 +4,11 @@ function execSQLQuery(sqlQry, res){
     const connection = mysql.createConnection({
       host     : 'localhost',
       port     : 3306,
-      user     : 'hick',
-      password : 'root',
+      user     : 'michel',
+      password : 'password',
       database : 'seinfo'
     });
-  
+
     connection.query(sqlQry, function(error, results, fields){
         if(error)
           res.json(error);
