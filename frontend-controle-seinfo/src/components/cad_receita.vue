9863f7ba3026e46ab4a5ba935841bba179f72bbd
JulianoCP <juliano-petini@hotmail.com>
2019-04-12 01:28:27 -0300

    Novas paginas e corringindo erros

---
+++ frontend-controle-seinfo/src/components/cad_receita.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>FAZER O CAD DE RECEITA</h1>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
