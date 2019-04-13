9f46cc0751904c61322f994937703eb07e4ba97f
rmmenezes <ra29fa@gmail.com>
2019-04-13 20:39:26 -0300

    frontend - Cadastro de Despesas

--- frontend-controle-seinfo/src/components/cad_despesa.vue
+++ frontend-controle-seinfo/src/components/cad_despesa.vue
@@ -1,6 +1,41 @@
 <template>
-<div>
-    <h1>FAZER O CAD DE DESPESA</h1>
+<div class="title">
+  <h5 style="text-align:center">Cadastro de Despesa</h5>
+  <div class="box">
+    <form class="form" action="http://localhost:3000/despesa" method="post">
+      <div class="row justify-content-center">
+        <a-form-item class="space_2">
+          <label class="ant-form-item-required">Nome da Despesa:</label>
+          <a-input placeholder="Nome" name="nome_desp" type="text">
+            <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)"/>
+          </a-input>
+        </a-form-item>
+      </div>
+      <div class="row justify-content-center">
+       <a-form-item class="space">
+          <label class="ant-form-item-required">Quantidade:</label>
+          <a-input placeholder="Quantidade" name="quantidade_desp" type="number">
+            <a-icon slot="prefix" type="shopping" style="color:rgba(0,0,0,.25)"/>
+          </a-input>
+        </a-form-item>
+        <a-form-item class="space">
+          <label class="ant-form-item-required">Valor da Despesa:</label>
+          <a-input placeholder="Valor" name="valor_desp" type="number">
+            <a-icon slot="prefix" type="dollar" style="color:rgba(0,0,0,.25)"/>
+          </a-input>
+        </a-form-item>
+      </div>
+      <div class="row justify-content-center">
+        <a-form-item class="space_2">
+          <label class="ant-form-item-required">Descrição:</label>
+          <a-textarea type="text" name="descricao_desp" placeholder="Descrição" :rows="4"/>
+        </a-form-item>
+      </div>
+      <div class="row justify-content-center">
+      <button type="submit" class="btn btn-outline-primary btn-sm">Cadastrar</button>
+      </div>
+    </form>
+  </div>
 </div>
 </template>
 
@@ -11,7 +46,37 @@ export default {
   }
 };
 </script>
-
 <style scoped>
+
+.box {
+  margin-top: 10px;
+  border-left: solid 1px rgba(161, 161, 161, 0.233);
+  border-right: solid 1px rgba(161, 161, 161, 0.233);
+  margin-left: 25%;
+  margin-right: 25%;
+  padding: 20px;
+}
+
+.title{
+  margin-top: 10px;
+}
+
+.space {
+  padding: 2px;
+  width: 221px;
+}
+
+label {
+  margin-bottom: 0;
+}
+
+.ant-form-item {
+  margin-bottom: 0;
+}
+
+.space_2 {
+  padding: 2px;
+  width: 444px;
+}
 
 </style>
