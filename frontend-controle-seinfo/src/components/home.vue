733c1b3e2cfd3e2e505e69639cd128c15cde2acc
JulianoCP <juliano-petini@hotmail.com>
2019-04-16 00:33:43 -0300

    frontend - Novo Layout da home

--- frontend-controle-seinfo/src/components/home.vue
+++ frontend-controle-seinfo/src/components/home.vue
@@ -1,26 +1,45 @@
 <template>
-  <div>
-    <navHome></navHome>
-    <workspace></workspace>
-    <rodape></rodape>
-  </div>
+  <a-layout id="components-layout-demo-top" class="layout">
+    <a-layout-header>
+       <div class="logo"><a href="http://localhost:8080/"><h5>SEINFO</h5></a></div>
+      <a-menu class="menu" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+        <a-menu-item key="1">LOGIN
+          <router-link to="/login"></router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout-content>
+      <workspace></workspace>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script>
 import workspace from "./workspace.vue";
-import navHome from "./navHome.vue";
 export default {
-    components:{
-        workspace,
-        navHome
-    }
+  components: {
+    workspace
+  }
 };
 </script>
 
-<style scoped>
+<style>
 
-.nav-link{
-  color: rgb(145, 145, 145);
+.menu{
+  text-align: end;
+}
+
+h5{
+  color: white;
+  margin: 0;
+}
+
+#components-layout-demo-top .logo {
+  width: 120px;
+  height: 31px;
+  margin: 20px 24px 10px;
+  float: left;
 }
 
 </style>
+
