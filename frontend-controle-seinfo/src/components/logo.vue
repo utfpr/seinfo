5843407b20ede079048381ab4cc97624d0fdd9e7
JulianoCP <juliano-petini@hotmail.com>
2019-04-17 19:11:43 -0300

    frontend - Add Logo

---
+++ frontend-controle-seinfo/src/components/logo.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="logo">
+  <img src="../assets/utfpr.jpg" >
+</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+<style scoped>
+
+.logo{
+  margin-top: 80px;
+  text-align: center;
+}
+
+</style>
