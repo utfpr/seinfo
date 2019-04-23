d20ab86e4c37fabefe3bdbea141bcf9721969abd
JulianoCP <juliano-petini@hotmail.com>
2019-04-23 19:53:06 -0300

    frontend - Ajustes

--- frontend-controle-seinfo/src/components/workspace.vue
+++ frontend-controle-seinfo/src/components/workspace.vue
@@ -8,15 +8,15 @@
   <img
     class="box-2"
     alt="example"
-    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    src="../assets/se.jpeg"
     slot="cover"
   />
   <template class="ant-card-actions" slot="actions">
-    <a-button class="bt">Default</a-button>
+    <a-button class="bt">INFORMAÇÕES</a-button>
   </template>
   <a-card-meta
-    title="Card title"
-    description="This is the description">
+    title="SEINFO 2019"
+    description="A Semana de Informática da UTFPR-CM, atualmente em sua sexta edição, é um evento voltado aos estudantes e profissionais na área de Informática da cidade de Campo Mourão e região, direcionado, principalmente, aos acadêmicos dos cursos de Tecnologia em Sistemas para Internet e Ciência da Computação da UTFPR-CM. O evento propicia aos participantes uma visão do mercado de trabalho, por meio do contato com palestrantes de outros estados e grandes empresas do país, além da troca de experiências com professores e egressos dos cursos de Informática da UTFPR-CM. Além disso, os participantes também conseguem adquirir uma visão da área acadêmica, graças à participação em seminários, palestras e minicursos.">
   </a-card-meta>
 </a-card>
 </div>
@@ -28,15 +28,15 @@
   <img
     class="box-2"
     alt="example"
-    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    src="../assets/se.jpeg"
     slot="cover"
   />
   <template class="ant-card-actions" slot="actions">
-    <a-button class="bt">Default</a-button>
+    <a-button class="bt">INFORMAÇÕES</a-button>
   </template>
   <a-card-meta
-    title="Card title"
-    description="This is the description">
+    title="SEINFO 2019"
+    description="A Semana de Informática da UTFPR-CM, atualmente em sua sexta edição, é um evento voltado aos estudantes e profissionais na área de Informática da cidade de Campo Mourão e região, direcionado, principalmente, aos acadêmicos dos cursos de Tecnologia em Sistemas para Internet e Ciência da Computação da UTFPR-CM. O evento propicia aos participantes uma visão do mercado de trabalho, por meio do contato com palestrantes de outros estados e grandes empresas do país, além da troca de experiências com professores e egressos dos cursos de Informática da UTFPR-CM. Além disso, os participantes também conseguem adquirir uma visão da área acadêmica, graças à participação em seminários, palestras e minicursos.">
   </a-card-meta>
 </a-card>
 </div>
@@ -50,28 +50,27 @@ export default {
 
 <style scoped>
 
+.texto{
+  text-align: center;
+}
+
 .box-1{
   height: 450px;
   width: 450px;
-  padding: 10px;
+  padding: 5px;
   margin: 50px;
   margin-top: 30px;
   background: transparent;
   border: transparent;
 }
 
+.ant-card-meta-title {
+  text-align: center;
+}
+
 .bt{
   border-color:blue;
   color: blue;
-}
-
-.ant-card-actions {
-    border-top: transparent;
-    background: transparent;
-    zoom: 1;
-    list-style: none;
-    margin: 0;
-    padding: 0;
 }
 
 .box-2{
