72894d25583e1b34bb394b4d404703ec597c015f
rmmenezes <ra29fa@gmail.com>
2019-05-08 20:54:18 -0300

    adicionado laço repetiçao para popular lista

---
+++ frontend-controle-seinfo/src/components/teste.vue
@@ -0,0 +1,23 @@
+<template>
+<select name="LeaveType" @change="onChange($event)" class="form-control" v-model="key">
+   <option value="1">Annual Leave/ Off-Day</option>
+   <option value="2">On Demand Leave</option>
+</select>   
+</template>
+
+<script>
+export default {
+  methods: {
+    onChange:function(event){
+       console.log(event.target.value);
+    }
+  },
+  data() {
+    return {
+      data,
+      columns,
+      selected: "",
+    }
+  }
+}
+</script>
