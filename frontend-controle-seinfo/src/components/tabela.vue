60275d9704d8bd0cd73d98b0fd3de639696b65b3
Rafael Menezes Barboza <ra29fa@gmail.com>
2019-05-20 15:25:27 -0300

    metodos de busca para listar relatorios

--- frontend-controle-seinfo/src/components/tabela.vue
+++ frontend-controle-seinfo/src/components/tabela.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <a-select defaultValue="Selecione uma Opção" class="space" @change="onchange()" v-model="key">
-        <a-select-option name="administradores" value="administradores">Administradores</a-select-option>
-        <a-select-option name="alunos" value="alunos">Alunos</a-select-option>
-        <a-select-option name="convidados" value="convidados">Convidados</a-select-option>
-        <a-select-option name="eventos" value="eventos">Eventos</a-select-option>
-        <a-select-option name="palestrantes" value="palestrantes">Palestrantes</a-select-option>
-        <a-select-option name="ministrantes" value="ministrantes">Ministrantes</a-select-option>
+    <a-select defaultValue="Selecione uma Opção" class="space">
+        <a-select-option name="administradores"  value="administradores" v-on:click="pegar_tabela()">Administradores</a-select-option>
+        <a-select-option name="alunos" value="alunos" v-on:click="pegar_tabela()">Alunos</a-select-option>
+        <a-select-option name="convidados" value="convidados" v-on:click="pegar_tabela()">Convidados</a-select-option>
+        <a-select-option name="eventos" value="eventos" v-on:click="pegar_tabela('eventos')">Eventos</a-select-option>
+        <a-select-option name="palestrantes" value="palestrantes" v-on:click="pegar_tabela()">Palestrantes</a-select-option>
+        <a-select-option name="ministrantes" value="ministrantes" v-on:click="pegar_tabela()">Ministrantes</a-select-option>
+
       </a-select>
 
     <a-table :columns="columns" :dataSource="data" :pagination="{ pageSize: 15 }" :scroll="{ y: 380 }" >
@@ -43,6 +44,17 @@ const columns = [{
 
 export default {
   methods: {
+    pegar_tabela (name) {
+      axios.get('http://localhost:3000/api/' + name)
+      .then((response) => {
+       console.log("Listou " + name);
+       console.log(response.data);
+       this.tabelas = response.data;
+     })
+     .catch(function (error) {
+       console.log(error);
+     })
+    },
     onchange: function() {
     	console.log(this.key)
       alert(this.key)
@@ -62,9 +74,11 @@ export default {
       data: [],
       columns,
       key: "Selecione...",
+      tabelas: [],
     }
   }
 }
+const axios = require('axios');
 </script>
 
 
@@ -76,7 +90,6 @@ export default {
   cursor: pointer;
   color: black;
 }
-
 .eve{
   left: 85%;
   margin-top: 2%;
