b2efa902be7f25ef4991603d399b966287125a84
Luiz Henrique <luiz_hickbarros@hotmail.com>
2019-06-26 19:57:23 -0300

    Conexão com API
    Criando conexão com a API Ldap

---
+++ backend-controle-seinfo/controllers/autenticacao.controller.js
@@ -0,0 +1,147 @@
+const db = require('../models/index.js');
+
+ 
+// Post do Atividade
+exports.create = (req, res) => {
+  
+    Autenticacao.create({  
+    senha:req.body.titulo,
+    ra: req.body.valor
+
+  }).then(atividade => {
+    atividade.createAtvAgenda({'dataHoraInicio':data_ini_full,'dataHoraFim':data_fim_full,'local':req.body.local})    
+    // Cria um Evento
+    console.log("Criado uma categoria!")
+    res.send(atividade);
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+};
+ 
+
+
+exports.findById = (req, res) => {  
+  Atividades.findOne({where:{idAtividade: req.params.idAtividade,idEvento:req.params.idEvento},include:[{model:db.categoria,as:'categoriaAtv'}]}).then(atividade => {
+    console.log("Achou uma atividade pelo ID "+req.params.idAtividade);
+    res.send(atividade); //Retorna um Json para a Pagina da API
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+};
+
+exports.findAll = (req, res) => {  
+  Atividades.findAll({ raw: true,include:[{model:db.categoria,as:'categoriaAtv'}]}).then(atividade => {
+    console.log("Listou Todas as Atividades!");
+    res.send(atividade); //Retorna um Json para a Pagina da API
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+};
+
+exports.atualiza = (req,res)=>{
+
+  Atividades.update(
+    {
+      titulo:req.body.titulo,
+      valor: req.body.valor,
+      descricao: req.body.descricao,
+      horasParticipacao: req.body.horasParticipacao,
+      quantidadeVagas:req.body.vagas,
+  },
+    {where: {idAtividade: req.params.idAtividade}}).then(atividade=>{    
+      console.log("Atualizando uma Atividade");
+      res.send(atividade);
+    }).catch(err=>{
+      res.status(500).send("Error "+err);
+    })
+    
+  },
+  
+
+exports.delete = (req, res) => {
+  Atividades.findOne({where:{idAtividade: req.params.idAtividade,idEvento:req.params.idEvento}}).then(atividade => {
+    db.agendamentoAtividade.findOne({where:{idAtividade:req.params.idAtividade}}).then(agenda=>{
+      db.agenda.destroy({where:{idAgenda:agenda.idAgenda}})
+      agenda.destroy()
+    })
+    db.protagonista.destroy({where:{idAtividade:req.params.idAtividade}})
+    atividade.destroy({where:{idAtividade: req.params.idAtividade}})
+    console.log("Achou uma atividade pelo ID "+req.params.idAtividade);
+    //res.status(204).send()
+    res.send('deletou'); //Retorna um Json para a Pagina da API
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })  
+};
+
+exports.AtividadeEvento=(req,res)=>{
+  //seleciona as atividades de um unico evento
+  Atividades.findAll({where:{idEvento:req.params.idEvento},include:[{model:db.categoria,as:'categoriaAtv'}]}).then(atividade => {
+    console.log("Listou Todas as Atividades!");
+    res.send(atividade); //Retorna um Json para a Pagina da API
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+}
+
+//-------------------------------------------------------------------------------------------
+
+exports.criarProtagonista = (req,res)=>{
+  Atividades.findOne({where:{idAtividade: req.params.idAtividade}}).then(atividade => {
+    db.pessoa.findOne({where:{idPessoa: req.params.idPessoa}}).then(pessoa=>{
+      db.protagonista.create({'atuacao':req.body.atuacao,'idAtividade':atividade.idAtividade,'idPessoa':pessoa.idPessoa}).then(prot=>{
+        res.send(prot)
+      })      
+      //atividade.createAtividadeProt({'atuacao':req.body.atuacao,'idAtividade':atividade.idAtividade,'idPessoa':pessoa.idPessoa})
+    }).catch(err=>{
+      res.status(500).send("Error -> " + err);
+    })
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+}
+
+exports.selectProtagonista=(req,res)=>{
+  //seleciona todos protagonistas no banco de dados
+  db.protagonista.findAll({raw:true}).then(prot=>{
+    res.send(prot)
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+  
+}
+
+exports.selectUmProtagonista=(req,res)=>{
+  //seleciona um protagonista de uma atividade
+  db.protagonista.findOne({where:{idAtividade:req.params.idAtividade,idPessoa:req.params.idPessoa}}).then(prot=>{
+    res.send(prot)
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+}
+
+exports.ProtagonistasDaAtv=(req,res)=>{
+  //seleciona os protagonistas de uma atividade especifica
+  db.protagonista.findAll({where:{idAtividade:req.params.idAtividade}}).then(prot=>{
+    res.send(prot)
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+}
+
+exports.AtividadesDoProtagonista=(req,res)=>{
+  //seleciona as atividades em que essa pessoa é protagonista
+  db.protagonista.findAll({where:{idPessoa:req.params.idPessoa}}).then(prot=>{
+    res.send(prot)
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+}
+
+exports.deletaProtagonista=(req,res)=>{
+  db.protagonista.destroy({where:{idAtividade:req.params.idAtividade,idPessoa:req.params.idPessoa}}).then(prot=>{
+    res.send('sumiu')
+  }).catch(err => {
+    res.status(500).send("Error -> " + err);
+  })
+}
