f2ff3aaa26829a4fdab060e98dd13ffbaee79446
Luiz Henrique <luiz_hickbarros@hotmail.com>
2019-06-27 23:53:06 -0300

    Configurando tempo de autenticação so sistema na API Ldap

--- backend-controle-seinfo/routes/autenticacao.route.js
+++ backend-controle-seinfo/routes/autenticacao.route.js
@@ -1,19 +1,17 @@
 module.exports = function(app) {
- 
     const login = require('../controllers/autenticacao.controller.js');
 
+    setTimeout(autenticar_API,1);
+    setInterval(autenticar_API,1000*60*60*23); // EXECUTA A API EM INTERVALO DE TEMPO (EM MILISEGUNDOS)
 
-    setInterval(nada2,10000); // EXECUTA A API EM INTERVALO DE TEMPO (EM MILISEGUNDOS)
-
-    function nada2(){
+    function autenticar_API(){
         // console.log("Executando API")
         login.autenticar();
     }
 
     // app.post('/api/autentication/',login.autentica);
-    
     app.post('/api/login/',login.login);
-  
+    
 }
 
 
