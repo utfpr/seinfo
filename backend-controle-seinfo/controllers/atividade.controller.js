52ce0bbbbf4e9a36c74747a64ff1006fb52d1dc3
MSechineli <49240681+MSechineli@users.noreply.github.com>
2020-08-13 16:29:13 -0300

    autenticação externa

--- backend-controle-seinfo/controllers/atividade.controller.js
+++ backend-controle-seinfo/controllers/atividade.controller.js
@@ -16,6 +16,7 @@ exports.create = (req, res) => {
 
   }).then(atividade => {
     atividade.createAtvAgenda({'dataHoraInicio':data_ini_full,'dataHoraFim':data_fim_full,'local':req.body.local})    
+    
     // Cria um Evento
     console.log("Criado uma categoria!")
     res.send(atividade);
