73f69df9ff48804be2361f6ec25976355dc9c23c
MSechineli <49240681+MSechineli@users.noreply.github.com>
2020-08-13 16:19:54 -0300

    Revert "autenticação externa"
    This reverts commit e7d08a2f0740f0b7d386ee462d85ec98dc63923f.

--- backend-controle-seinfo/routes/autenticacao.route.js
+++ backend-controle-seinfo/routes/autenticacao.route.js
@@ -1,15 +1,15 @@
 module.exports = function(app) {
     const login = require('../controllers/autenticacao.controller.js');
 
-    // setTimeout(autenticar_API,1);
-    // setInterval(autenticar_API,1000*60*60*23); // EXECUTA A API EM INTERVALO DE TEMPO (EM MILISEGUNDOS)
+    setTimeout(autenticar_API,1);
+    setInterval(autenticar_API,1000*60*60*23); // EXECUTA A API EM INTERVALO DE TEMPO (EM MILISEGUNDOS)
 
-    // function autenticar_API(){
-    //     // console.log("Executando API")
-    //     login.autenticar();
-    // }
+    function autenticar_API(){
+        // console.log("Executando API")
+        login.autenticar();
+    }
 
-    // // app.post('/api/autentication/',login.autentica);
+    // app.post('/api/autentication/',login.autentica);
     app.post('/api/login/',login.login);
     
 };
