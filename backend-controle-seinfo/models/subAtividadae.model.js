3c2669d032c01f807dc5e9bb76dee6eaa46cdd70
Andre Monegat <andre.monegat@gmail.com>
2021-05-02 16:46:50 -0300

    adicionando modelo de subatividade

---
+++ backend-controle-seinfo/models/subAtividadae.model.js
@@ -0,0 +1,69 @@
+module.exports = (sequelize, Sequelize) => {
+    const SubAtividade = sequelize.define('agendamentoAtividade', {
+        idAtividade: {
+            type: Sequelize.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: 'atividade',
+                key: 'idAtividade'
+            },
+            field: 'idAtividade'
+        },
+        idAgenda: {
+            type: Sequelize.INTEGER(11),
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: 'agenda',
+                key: 'idAgenda'
+            },
+            field: 'idAgenda'
+        },
+        idParticipante: {
+            type: Sequelize.STRING(64),
+            allowNull: false,
+            field: 'idParticipante'
+        },
+        presente:{
+            type: Boolean,
+            default: false,
+            allowNull: false,
+            field: 'presenca'
+        }
+    },
+        { //Isso serve para não recriar a tabela e impedir de recriar esses atributos setados como false(timestamps,createdAt)
+            tableName: 'agendamentoAtividade',
+            timestamps: false,
+            createdAt: false,
+        });
+
+    AgAtividade.associate = models => {
+
+        models.atividade.belongsToMany(models.agenda, {
+            as: 'atvAgenda',
+            through: models.agendamentoAtividade,
+            foreignKey: 'idAtividade',
+            onUpdate: 'cascade',
+            onDelete: 'cascade',
+        }),
+
+            models.agenda.belongsToMany(models.atividade, {
+                as: 'agendaAtv',
+                through: models.agendamentoAtividade,
+                foreignKey: 'idAgenda',
+                onUpdate: 'cascade',
+                onDelete: 'cascade',
+            })
+        models.agenda.belongsToMany(models.atividade, {
+            as: 'agendaAtv',
+            through: models.agendamentoAtividade,
+            foreignKey: 'idAgenda',
+            onUpdate: 'cascade',
+            onDelete: 'cascade',
+        })
+    };
+
+
+    return SubAtividade;
+}
