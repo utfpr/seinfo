3fcf23b35f8ff0d92cdc4bd4e89e98eab1fadeb1
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-21 17:52:53 -0300

    :sparkles: Atualizando pacote mysql.

--- backend-controle-seinfo/models/index.js
+++ backend-controle-seinfo/models/index.js
@@ -4,8 +4,8 @@ const Sequelize = require('sequelize');
 const config = require('../config/env');
 
 const db = {};
-const sequelize = new Sequelize(config.env);
-
+const sequelize = new Sequelize(config);
+console.log(sequelize)
 fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
