8f84e1ab13f02d577a9060cc2b826651cc2af7c2
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-06-23 16:46:29 -0300

    :lipstick: Adicionando prettier/eslint.

--- backend-controle-seinfo/app.js
+++ backend-controle-seinfo/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["localhost:8080"],
+    origin: ["http://localhost:8080"],
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,16 +14,17 @@ app.use(bodyParser.json());
 
 const router = express.Router();
 
-//Chamo todas as minhas Rotas aqui
-require("./routes/evento.route.js")(app);
-require("./routes/lote.route.js")(app);
-require("./routes/categoria.route.js")(app);
-require("./routes/atividade.route.js")(app);
-require("./routes/agenda.route.js")(app);
-require("./routes/imagem.route.js")(app);
-require("./routes/carrossel.route.js")(app);
-require("./routes/autenticacao.route.js")(app);
-require("./routes/pessoa.route.js")(app);
+// Chamo todas as minhas Rotas aqui
+require("./routes/evento.route")(app);
+require("./routes/lote.route")(app);
+require("./routes/categoria.route")(app);
+require("./routes/atividade.route")(app);
+require("./routes/agenda.route")(app);
+require("./routes/imagem.route")(app);
+require("./routes/carrossel.route")(app);
+require("./routes/autenticacao.route")(app);
+require("./routes/pessoa.route")(app);
+
 app.use("/", router);
 
 module.exports = app;
