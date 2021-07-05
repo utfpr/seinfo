eae3bffbad64995481e7b95dc35dda613af8f015
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-07-05 16:24:59 -0300

    :tada: Atualizando versão do vue, utilizando vue-cli.

--- frontend-controle-seinfo/config/dev.env.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-
-const prodEnv = require('./prod.env');
-const dotEnv = require('./dotenv.env');
-
-module.exports = merge(prodEnv, dotEnv, {
-  NODE_ENV: '"production"',
-});
---
+++ frontend-controle-seinfo/vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+  runtimeCompiler: true,
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+}
