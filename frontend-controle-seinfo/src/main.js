05de4ab1eea86b149f0806a7872f4d97b4bb4444
Ricardo Fedrigo <https://RicardoFedrigo@github.com>
2021-07-21 21:13:23 -0300

    hotfix: Arrumando rotas

--- frontend-controle-seinfo/src/main.js
+++ frontend-controle-seinfo/src/main.js
@@ -1,34 +1,50 @@
-import Vue from 'vue';
-import App from './App';
-import teste from './router/teste';
-import Admin from './router/admin';
-import Login from './router/login';
-import Usuario from './router/usuario';
-import 'ant-design-vue/dist/antd.css';
-import Antd from 'ant-design-vue';
+import Vue from "vue";
+import App from "./App";
+import teste from "./router/teste";
+import Admin from "./router/admin";
+import Login from "./router/login";
+import "ant-design-vue/dist/antd.css";
+import Antd from "ant-design-vue";
 import SlideUpDown from "vue-slide-up-down";
-import VueTheMask from 'vue-the-mask';
+import VueTheMask from "vue-the-mask";
+import Usuario from "./router/usuario";
 
 const app_url = process.env.VUE_APP_URL;
 
-Vue.component('slide-up-down', SlideUpDown);
+Vue.component("slide-up-down", SlideUpDown);
 Vue.use(Antd);
 Vue.use(VueTheMask);
 
-var router; 
+let router;
+
+const superVisor = () => {}
+
+const perm = [Usuario,superVisor,Admin]
 
 Vue.config.productionTip = false;
 
 setTimeout(() => {
   teste.userData().then((res) => {
-    router = res !== null ? (res.nivel === 1 ? Usuario : Admin) : Login;
+    router = res ?  perm[res.nivel]: Login;
+
+    // router = (res) => {
+    //   const { nivel } = res.nivel
+    //   if (res.nivel === 0) {
+    //     return Usuario;
+    //   } else if (res.nivel === 1) {
+    //     return Login;
+    //   } else if(res.){
+    //     return Admin;
+    //   }
+    //   return Login;
+    // };
     new Vue({
-      el: '#app',
+      el: "#app",
       router,
       components: { App },
-      template: '<App/>'
+      template: "<App/>",
     });
-  })
+  });
 }, 500);
 
 export default app_url;
